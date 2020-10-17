ecb554f4c056e01e01aeff8fa2b79eff6f4bccce
Joycelyn Hicks <joyhicks23@gmail.com>
2020-10-17 08:34:21 -0600

    added inputs

---
+++ Desktop/project-3/src/App.js
@@ -0,0 +1,22 @@
+import React from 'react';
+import './App.css';
+
+function App() {
+  return (
+    <div>
+      <input type="checkbox" />
+       <p>Placeholder text</p>
+
+       <input type="checkbox" />
+       <p>Placeholder text</p>
+
+       <input type="checkbox" />
+       <p>Placeholder text</p>
+
+       <input type="checkbox" />
+       <p>Placeholder text</p>
+    </div>
+  )
+}
+
+export default App;
